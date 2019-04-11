2c9e5711a74068e18e4136a3f272f36cac7cb138
INTERNAL\dmitriy.a <dmitriy.a@tabella.cz>
2019-04-11 10:52:53 +0200

    move loadingpage to separate folder

---
+++ src/pages/Loadingpage/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { ReactComponent as Logo } from '../../images/logo.svg';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const spinKeyframes = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+const SpinningBlock = styled.div`
+  width: 320px;
+  transform: rotate(0deg);
+  animation: ${spinKeyframes} 3s linear infinite;
+`;
+
+const Loadingpage = () => {
+  return (
+    <Wrapper>
+      <SpinningBlock>
+        <Logo />
+      </SpinningBlock>
+    </Wrapper>
+  );
+};
+
+export default Loadingpage;
