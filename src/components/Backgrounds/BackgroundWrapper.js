003618d2f72f3b669c1f470357096a63d530515f
INTERNAL\dmitriy.a <dmitriy.a@tabella.cz>
2019-04-11 10:54:31 +0200

    improve BackgroundWrapper

--- src/components/Backgrounds/BackgroundWrapper.js
+++ src/components/Backgrounds/BackgroundWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const BackgroundWrapper = styled.div`
   position: fixed;
-  z-index: ${props => props.zIndex};
+  z-index: ${props => Math.min(props.zIndex, -1)};
   background: ${props => props.backgroundColor};
   overflow: hidden;
   top: 0;
