0ca391cd38fd5da89910074664c05586104830ba
INTERNAL\dmitriy.a <dmitriy.a@tabella.cz>
2019-04-16 17:08:29 +0200

    Updates

---
+++ precache-manifest.2064d8ed9e4d9ebdcd3220818b0ef29e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a504f29d46465613c0bfd85e4f70b7",
+    "url": "/alice-in-wonderland/static/media/alice.d7a504f2.png"
+  },
+  {
+    "revision": "670b29d77552f7d95c07",
+    "url": "/alice-in-wonderland/static/css/main.ac6ce8a6.chunk.css"
+  },
+  {
+    "revision": "00db5345baf265cc5416",
+    "url": "/alice-in-wonderland/static/js/runtime~main.503df334.js"
+  },
+  {
+    "revision": "87984351d22d42ad2309",
+    "url": "/alice-in-wonderland/static/js/2.67bdf336.chunk.js"
+  },
+  {
+    "revision": "6161d90025c5403d80e1b7d3a66dd82d",
+    "url": "/alice-in-wonderland/static/media/wonderland-logo.6161d900.png"
+  },
+  {
+    "revision": "cb7a498799ab1a827177395cfc4bdcff",
+    "url": "/alice-in-wonderland/static/media/whiteRabbit.cb7a4987.png"
+  },
+  {
+    "revision": "0ca85a990fb30bb736a901482e62226c",
+    "url": "/alice-in-wonderland/static/media/caterpillar.0ca85a99.png"
+  },
+  {
+    "revision": "c718c449584088e0d122d0a9a518c9a3",
+    "url": "/alice-in-wonderland/static/media/madHatter.c718c449.png"
+  },
+  {
+    "revision": "2f49a564a2b1f69492bf9241f56281e8",
+    "url": "/alice-in-wonderland/static/media/alice.2f49a564.png"
+  },
+  {
+    "revision": "bf96b07ec9494c6fdf98cbf0bb0736ff",
+    "url": "/alice-in-wonderland/static/media/cheshireCat.bf96b07e.png"
+  },
+  {
+    "revision": "77ebd61a103f62985a43775691f0ed42",
+    "url": "/alice-in-wonderland/static/media/queenOfHearts.77ebd61a.png"
+  },
+  {
+    "revision": "39f843380bb79e7715e4f7e5ded00db1",
+    "url": "/alice-in-wonderland/static/media/whiteRabbit.39f84338.png"
+  },
+  {
+    "revision": "504d697c2c71f48e4b2132f3364c612a",
+    "url": "/alice-in-wonderland/static/media/caterpillar.504d697c.png"
+  },
+  {
+    "revision": "a5c7c71a1caaf6cba5c64aa58e87d3fa",
+    "url": "/alice-in-wonderland/static/media/madHatter.a5c7c71a.png"
+  },
+  {
+    "revision": "670b29d77552f7d95c07",
+    "url": "/alice-in-wonderland/static/js/main.7cfdd303.chunk.js"
+  },
+  {
+    "revision": "c7965aa33347f31f72f940265d1b0a27",
+    "url": "/alice-in-wonderland/static/media/cheshireCat.c7965aa3.png"
+  },
+  {
+    "revision": "4148c24205c3c851ba34aa52aadc9d63",
+    "url": "/alice-in-wonderland/static/media/queenOfHearts.4148c242.png"
+  },
+  {
+    "revision": "f568484bf0dcadd6013c067790935d7a",
+    "url": "/alice-in-wonderland/static/media/whiteRabbit-cta.f568484b.png"
+  },
+  {
+    "revision": "3794cc4cf05144a52df742872a695c9c",
+    "url": "/alice-in-wonderland/static/media/caterpillar-cta.3794cc4c.png"
+  },
+  {
+    "revision": "aae902da44420c01f7a66d0ba3085116",
+    "url": "/alice-in-wonderland/static/media/madHatter-cta.aae902da.png"
+  },
+  {
+    "revision": "9c402aca4888c4192f6eb887aea17e0c",
+    "url": "/alice-in-wonderland/static/media/alice-cta.9c402aca.png"
+  },
+  {
+    "revision": "e967fc3c0640d8c8ef9685d051b1b0e0",
+    "url": "/alice-in-wonderland/static/media/cheshireCat-cta.e967fc3c.png"
+  },
+  {
+    "revision": "d195ffc0f3c7c8290de63440c68d5ae1",
+    "url": "/alice-in-wonderland/static/media/queenOfHearts-cta.d195ffc0.png"
+  },
+  {
+    "revision": "9169ac38555c33174035ba1a65ff8c00",
+    "url": "/alice-in-wonderland/static/media/mainBackground.9169ac38.jpg"
+  },
+  {
+    "revision": "b80a2ebd65e5ece681478641f1762a09",
+    "url": "/alice-in-wonderland/static/media/cardsBackground.b80a2ebd.png"
+  },
+  {
+    "revision": "349661de29c206919b5d16edf053df19",
+    "url": "/alice-in-wonderland/static/media/thingsBackground.349661de.png"
+  },
+  {
+    "revision": "80f7a3322a79bfc3d5cc8458c8aaacbb",
+    "url": "/alice-in-wonderland/static/media/logo.80f7a332.png"
+  },
+  {
+    "revision": "bd1394dbd4144982e837b9787f404fcd",
+    "url": "/alice-in-wonderland/static/media/light.bd1394db.png"
+  },
+  {
+    "revision": "05d71f251e948c24526ad9a120db4c66",
+    "url": "/alice-in-wonderland/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/alice-in-wonderland/precache-manifest.112bdb360775d0464f3f6c061c7ac120.js"
+  "/alice-in-wonderland/precache-manifest.2064d8ed9e4d9ebdcd3220818b0ef29e.js"
 );
 
 workbox.clientsClaim();
