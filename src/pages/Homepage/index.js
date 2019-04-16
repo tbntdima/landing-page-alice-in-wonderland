170db8773af95eae858321ceef72e90071cc36d5
INTERNAL\dmitriy.a <dmitriy.a@tabella.cz>
2019-04-16 17:00:25 +0200

    improve optimization

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -191,16 +191,6 @@ class Homepage extends Component {
                   character => character.couponImage
                 )}
               />
-              <ImagesPreloader
-                imagesList={characters.list.map(
-                  character => character.couponCloseButton
-                )}
-              />
-              <ImagesPreloader
-                imagesList={characters.list.map(
-                  character => character.couponCTA
-                )}
-              />
             </div>
           );
         }}
